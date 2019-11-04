7b1ce05ad750a02843b9f67af407566ce665d298
rapha <raph2706@hotmail.com>
2019-11-03 18:17:44 -0800

    adicionando workspaces completos com robo seguindo parede

---
+++ simulation_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/raphacosta/simulation_ws/src
+# Build directory: /home/raphacosta/simulation_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(ag2)
+subdirs(my_worlds)
---
+++ simulation_ws/build/ag2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/raphacosta/simulation_ws/src/ag2
+# Build directory: /home/raphacosta/simulation_ws/build/ag2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
