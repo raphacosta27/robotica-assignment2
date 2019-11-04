e4ac744ebee5d6b68f3fce6ec2efe36d922ed6bd
rapha <raph2706@hotmail.com>
2019-11-04 02:51:46 -0800

    gitignore

--- simulation_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/raphacosta/simulation_ws/src
-# Build directory: /home/raphacosta/simulation_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(ag2)
-subdirs(my_worlds)
--- simulation_ws/build/ag2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/raphacosta/simulation_ws/src/ag2
-# Build directory: /home/raphacosta/simulation_ws/build/ag2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
